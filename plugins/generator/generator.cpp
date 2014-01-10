3ced431f84956d5e42f826299b444cdb3c8a6f7a
steffenmauch <steffen.mauch@gmail.com>
2014-01-10 22:35:53 +0100

    fixed copyright information in generator *.c and *.h

--- plugins/generator/generator.cpp
+++ plugins/generator/generator.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2013 by Steffen Mauch                                   *
+ *   Copyright (C) 2014 by Steffen Mauch                                   *
  *   steffen.mauch@gmail.com                                               *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
